63ac1e3c2b097606e0a8fac224efb20bca9dc407
sergio1008 <sergiobernalc@outlook.com>
2021-12-21 16:21:34 -0500

    Add files via upload
    add files

---
+++ src/main/java/com/example/demo/api/TestRestController.java
@@ -0,0 +1,14 @@
+package com.example.demo.api;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestRestController {
+	@GetMapping("/test")
+	public ResponseEntity<String> holaMundo(){
+		return new ResponseEntity<>("Hola", HttpStatus.OK);
+	}
+}
